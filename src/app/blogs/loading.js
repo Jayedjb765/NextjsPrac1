bac4b3590a64de91ac5220630274570c7dd550c0
Jayedjb765 <jayedjb76@gmail.com>
2025-05-26 11:17:52 +0600

    Adding blog

---
+++ src/app/blogs/loading.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LoadingPage = () => {
+    return (
+        <div>
+            <h2 className="text-3xl text-center text-red-600">Loading....</h2>
+
+        </div>
+    );
+};
+
+export default LoadingPage;
--- src/app/blogs/page.js
+++ src/app/blogs/page.js
@@ -1,8 +1,19 @@
+import Blog from '@/components/blog/page';
 import React from 'react';
 
-const BlogsPage = () => {
+const BlogsPage = async () => {
+    const res = await fetch('http://localhost:3004/blogs', {
+        cache: 'no-store'
+    })
+    const blogs = await res.json()
     return (
         <div>
+            <h2 className="text-2xl text-center font-bold">All Blogs</h2>
+            <div className="grid grid-cols-3 mx-auto ml-24 mt-10">
+                {
+                    blogs.map(blog => <Blog key={blog.id} blog={blog}></Blog>)
+                }
+            </div>
 
         </div>
     );
